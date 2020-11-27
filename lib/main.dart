b425dd489fa9464ce79919b3213000cc1c351b39
responilla07 <esponilla.dev404@gmail.com>
2020-11-27 18:09:29 +0800

    feat: Created charity model.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:donation_system/Variables/color.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -6,6 +7,7 @@ import 'Pages/RootPage.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_){
     runApp(new MaterialApp(
       theme: ThemeData(
