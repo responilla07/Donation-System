4b5e34b55ec60bb81aec7250168ae9b0b99e9bf9
responilla07 <esponilla.dev404@gmail.com>
2020-11-28 06:48:22 +0800

    feat: Created items in market card

---
+++ lib/Classes/MarketPageClass.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class MarketPageClass {
+  TextEditingController textController = TextEditingController();
+  ScrollController scrollController = ScrollController();
+  
+}
