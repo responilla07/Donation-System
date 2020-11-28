70b6be3e10bba789c00e2624ea18d9c6181d1217
responilla07 <esponilla.dev404@gmail.com>
2020-11-29 07:55:15 +0800

    fix: available soon text

--- lib/SubPages/MyItems.dart
+++ lib/SubPages/MyItems.dart
@@ -2,16 +2,16 @@ import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:donation_system/Classes/MyItemListClass.dart';
+import 'package:donation_system/Formatter/number.dart';
 import 'package:donation_system/Models/ItemModel.dart';
 import 'package:donation_system/SubPages/AddItem.dart';
-import 'package:donation_system/SubPages/ItemDetails.dart';
 import 'package:donation_system/Variables/color.dart';
 import 'package:donation_system/Variables/global.dart';
-import 'package:donation_system/Widgets/ItemCard.dart';
-import 'package:donation_system/Widgets/LoaderForPagination.dart';
 import 'package:donation_system/Widgets/SubPagesAppBar.dart';
 import 'package:donation_system/transitions/slide_route.dart';
 import 'package:flutter/material.dart';
+
+import 'MyItemsDetails.dart';
 
 class MyItems extends StatefulWidget {
   @override
@@ -202,61 +202,81 @@ class _MyItemsState extends State<MyItems> {
                     }
 
                     if (snapshot.connectionState == ConnectionState.done ){ marketClass.isFetching.value = false; }
-
                     return marketClass.listOfItems.value.length != 0 ? Expanded(
                       child: ListView.builder(
+                        padding: EdgeInsets.fromLTRB(15, 15, 15, 0),
                         itemBuilder: (context, position){
+                          ItemModel itemModel = marketClass.listOfItems.value[position];
                           return GestureDetector(
                             onTap: () async {
-                              await Navigator.push(context, SlideLeftRoute(page: ItemDetails(itemModel: marketClass.listOfItems.value[position],)));
+                              await Navigator.push(context, SlideLeftRoute(page: MyItemsDetails(itemModel: marketClass.listOfItems.value[position],)));
                             },
-                            child: Card(
-                              elevation: 8.0,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                              ),
-                              child: Container(
-                                height: 150,
-                                child: Column(
-                                  children: [
-
-                                    Row(
-                                      children: [
-                                        Expanded(
-                                          child: Container(
-                                            height: 150,
+                            child: Padding(
+                              padding: const EdgeInsets.only(bottom: 15),
+                              child: Card(
+                                elevation: 8.0,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                child: Container(
+                                  height: 120,
+                                  child: Column(
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        children: [
+                                          Container(
+                                            height: 120,
+                                            width: 120,
                                             decoration: BoxDecoration(
                                               borderRadius: BorderRadius.circular(10),
                                               image: DecorationImage(
                                                 image: NetworkImage(
-                                                  marketClass.listOfItems.value[position].itemPhoto[0]
+                                                  itemModel.itemPhoto[0]
                                                 ),
                                                 fit: BoxFit.cover
                                               )
                                             )
                                           ),
-                                        ),
-                                        Expanded(
-                                          child: Column(
-                                            children: [
-                                              Text(
-                                                "Name: "+ marketClass.listOfItems.value[position].name,
-                                              ),
-                                              Text(
-                                               "Price: "+ "P "+ marketClass.listOfItems.value[position].price.toString(),
-                                              ),
-                                              Text(
-                                                "Discount: "+"P " +marketClass.listOfItems.value[position].discounted.toString(),
-                                              ),
-                                              Text(
-                                                "Revieer: "+marketClass.listOfItems.value[position].ratings.toString()
-                                              ),
-                                            ],
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                  ],
+                                          SizedBox(width: 10,),
+                                          Expanded(
+                                            child: Column(
+                                              mainAxisAlignment: MainAxisAlignment.start,
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  itemModel.name,
+                                                  maxLines: 2,
+                                                  overflow: TextOverflow.ellipsis,
+                                                ),
+                                                Text(
+                                                  "₱ " + numberDecimalComma(itemModel.price),
+                                                  maxLines: 1,
+                                                  // overflow: TextOverflow.ellipsis,
+                                                ),
+                                                Row(
+                                                  children: [
+                                                    Icon(Icons.star, size: 15, color: itemModel.ratings >= 1 ? Colors.yellow : Colors.grey),
+                                                    Icon(Icons.star, size: 15, color: itemModel.ratings >= 2 ? Colors.yellow : Colors.grey),
+                                                    Icon(Icons.star, size: 15, color: itemModel.ratings >= 3 ? Colors.yellow : Colors.grey),
+                                                    Icon(Icons.star, size: 15, color: itemModel.ratings >= 4 ? Colors.yellow : Colors.grey),
+                                                    Icon(Icons.star, size: 15, color: itemModel.ratings >= 5 ? Colors.yellow : Colors.grey),
+                                                    SizedBox(width: 5,),
+                                                    Text(
+                                                      '('+ totalCount(itemModel.totalReviewer) + ')',
+                                                      style: TextStyle(
+                                                        fontSize: 12
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ],
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ),
                             ),
